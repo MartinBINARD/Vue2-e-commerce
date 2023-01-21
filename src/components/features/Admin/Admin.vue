48b23b62c92e8ed714716ff7ef45aba76f36b7ce
Martin BINARD <martin.binard.dev@gmail.com>
2023-01-21 21:43:26 +0100

    Create new file architecture

---
+++ src/components/features/Admin/Admin.vue
@@ -0,0 +1,16 @@
+<template>
+  <admin-product-form></admin-product-form>
+</template>
+
+<script>
+import AdminProductForm from './AdminProductForm.vue';
+
+export default {
+  components: {
+    AdminProductForm,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/features/User/User.vue
@@ -0,0 +1,18 @@
+<template>
+        <div class="d-flex flex-row">
+                <shop class="w-75"></shop>
+                <cart class="25"></cart>
+        </div>
+</template>
+
+<script>
+import Shop from './Shop/Shop.vue';
+import Cart from './Cart/Cart.vue';
+
+export default {
+
+}
+</script>
+
+<style>
+</style>
